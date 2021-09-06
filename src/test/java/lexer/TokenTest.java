a400241183f6d25cc5a70e81a6d2599263f37132
JiangTaoHou <925147954@qq.com>
2021-09-06 16:24:11 +0800

    用状态机来提取字符串，操作符，数字,测试通过

--- src/test/java/lexer/TokenTest.java
+++ src/test/java/lexer/TokenTest.java
@@ -22,6 +22,58 @@ class TokenTest {
         it1.next();
         var token3 = Token.makeVarOrKeyword(it1);
         assertToken(token3,"abc",TokenType.VARIABLE);
+
+    }
+    @Test
+    public void test_makeString() throws Exception{
+        String[] tests = {
+                "\"123\"",
+                "'123'"
+        };
+        for (String test : tests) {
+            var it = new PeekIterator<>(test.chars().mapToObj(x -> (char) x));
+            var token = Token.makeString(it);
+            assertToken(token,test,TokenType.STRING);
+        }
+    }
+    @Test
+    public void test_makeOperator() throws Exception{
+        String[] tests = {
+                "+ xxx",
+                "++mmm",
+                "/=g",
+                "==1",
+                "&=3982",
+                "&777",
+                "||xxx",
+                "^=111",
+                "%7"
+        };
+        int i = 0;
+        String[] results = {"+","++","/=","==","&=","&","||","^=","%"};
+        for (String test : tests) {
+            var it = new PeekIterator<>(test.chars().mapToObj(x ->(char)x));
+            var token = Token.makeOp(it);
+            assertToken(token,results[i++],TokenType.OPERATOR);
+        }
+    }
+
+    @Test
+    public void test_makeNumber() throws Exception{
+        String[] tests = {
+                "+0 aa",
+                "-0 aa",
+                ".3 ccc",
+                ".5555 ddd",
+                "7789.8888 ooo",
+                "-1000.123123*123123"
+        };
+        for (String test : tests) {
+            var it = new PeekIterator<Character>(test.chars().mapToObj(x->(char)x));
+            var token = Token.makeNumber(it);
+            var splitValue = test.split("[* ]+");
+            assertToken(token,splitValue[0], test.contains(".") ?TokenType.FLOAT:TokenType.INTEGER);
+        }
     }
 
 }
